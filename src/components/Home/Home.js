36d139b25a109cf6e217d7e15d78a12dace89eb2
minhazpro3 <minhazpro1@gmail.com>
2022-11-12 21:19:35 +0600

    setup project

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../Nav/Nav";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Nav.css";
+import image from "../images/image 1.png";
+import { FaChevronDown } from "react-icons/fa";
+import { HiUserCircle } from "react-icons/hi";
+
+const Nav = () => {
+  return (
+    <div style={{ backgroundColor: "#FFFFFF" }}>
+      <div className="header_container">
+        <div className="nav_subCon">
+          <div className="logo_img">
+            <img src={image} alt="" />
+          </div>
+          <div className="nav_routes">
+            <h5>My Assignment</h5>
+            <h5>Chat With Mentor</h5>
+            <div className="profile_route">
+              <HiUserCircle className="array_icon" />
+              <h5>Profile Name</h5>
+              <FaChevronDown className="array_icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
